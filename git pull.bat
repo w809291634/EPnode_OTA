e059187aa459fb186d6eeb0390f497a2176ff7ad
wanghao <809291634@qq.com>
2023-04-18 21:33:05 +0800

    gx

---
+++ git pull.bat
@@ -0,0 +1,14 @@
+@echo on
+cd C:\Users\Administrator\Desktop\Manual
+git pull origin
+cd C:\Users\Administrator\Desktop\study_doc
+git pull origin
+cd C:\Users\Administrator\Desktop\EPnode_OTA
+git pull origin
+cd C:\Users\Administrator\Desktop\WIFI8266_RTT
+git pull origin
+cd C:\Users\Administrator\Desktop\RFID_EXP
+git pull origin
+cd C:\Users\Administrator\Desktop\QT_Creator_Study
+git pull origin
+TIMEOUT /T 3
---
+++ git push.bat
@@ -0,0 +1,14 @@
+@echo on
+cd C:\Users\Administrator\Desktop\Manual
+git add . && git commit -m "gx" && git push origin
+cd C:\Users\Administrator\Desktop\study_doc
+git add . && git commit -m "gx" && git push origin
+cd C:\Users\Administrator\Desktop\EPnode_OTA
+git add . && git commit -m "gx" && git push origin
+cd C:\Users\Administrator\Desktop\WIFI8266_RTT
+git add . && git commit -m "gx" && git push origin
+cd C:\Users\Administrator\Desktop\RFID_EXP
+git add . && git commit -m "gx" && git push origin
+cd C:\Users\Administrator\Desktop\QT_Creator_Study
+git add . && git commit -m "gx" && git push origin
+TIMEOUT /T 10
