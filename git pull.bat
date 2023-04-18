476c491326d5ecfc54554def557ff7fac27ba879
wanghao <809291634@qq.com>
2023-04-18 21:59:56 +0800

    gx

--- git pull.bat
+++ git pull.bat
@@ -1,14 +1,15 @@
 @echo on
-cd C:\Users\Administrator\Desktop\Manual
+F:
+cd F:\1_git_code\EPnode_OTA
 git pull origin
-cd C:\Users\Administrator\Desktop\study_doc
+cd F:\1_git_code\Manual
 git pull origin
-cd C:\Users\Administrator\Desktop\EPnode_OTA
+cd F:\1_git_code\QT_Creator_Study
 git pull origin
-cd C:\Users\Administrator\Desktop\WIFI8266_RTT
+cd F:\1_git_code\RFID_EXP
 git pull origin
-cd C:\Users\Administrator\Desktop\RFID_EXP
+cd F:\1_git_code\study_doc
 git pull origin
-cd C:\Users\Administrator\Desktop\QT_Creator_Study
+cd F:\1_git_code\WIFI8266_RTT
 git pull origin
-TIMEOUT /T 3
+pause
--- git push.bat
+++ git push.bat
@@ -1,14 +1,15 @@
 @echo on
-cd C:\Users\Administrator\Desktop\Manual
+F:
+cd F:\1_git_code\EPnode_OTA
 git add . && git commit -m "gx" && git push origin
-cd C:\Users\Administrator\Desktop\study_doc
+cd F:\1_git_code\Manual
 git add . && git commit -m "gx" && git push origin
-cd C:\Users\Administrator\Desktop\EPnode_OTA
+cd F:\1_git_code\QT_Creator_Study
 git add . && git commit -m "gx" && git push origin
-cd C:\Users\Administrator\Desktop\WIFI8266_RTT
+cd F:\1_git_code\RFID_EXP
 git add . && git commit -m "gx" && git push origin
-cd C:\Users\Administrator\Desktop\RFID_EXP
+cd F:\1_git_code\study_doc
 git add . && git commit -m "gx" && git push origin
-cd C:\Users\Administrator\Desktop\QT_Creator_Study
+cd F:\1_git_code\WIFI8266_RTT
 git add . && git commit -m "gx" && git push origin
-TIMEOUT /T 10
+pause
