80109a4dd060925e5b6080ce82d026e2686b759b
汪浩 <wanghao@zonesion.comc.cn>
2023-04-21 10:22:38 +0800

    gx

---
+++ 提交代码/IAP-radio/IAP_For_NB/Project/IAP/console/command.h
@@ -0,0 +1,56 @@
+/*
+ * vivi/include/command.h
+ *
+ */
+#ifndef _VIVI_COMMAND_H_
+#define _VIVI_COMMAND_H_
+#include <string.h>
+#include <stdint.h>
+
+#define  MEMSET memset
+#define  STRNCMP strncmp
+#define  STRLEN  strlen
+#define  STRCAT  strcat
+
+#define BOOT_IAP    0xA2
+#define BOOT_APP    0x3C
+
+#define APP_OK      0x4D
+#define APP_ERR     0xFF
+
+#define HOP_TAB_SIZE	10
+typedef struct user_data {
+  uint16_t boot_mode;
+  uint16_t app_en;
+  uint32_t flag;
+  char mac[24];                 //模块mac
+  char zhiyun_id[40];           //智云id
+  char zhiyun_key[120];         //智云key
+  char ip[38];                  //连接IP
+  uint16_t port;                //连接Port
+}user_data_t;
+
+typedef struct user_command {
+	const char *name;
+	void (*cmdfunc)(int argc, const char **);
+	struct user_command *next_cmd;
+	const char *helpstr;
+} user_command_t;
+
+typedef struct user_subcommand {
+	const char *name;
+	void (*cmdfunc)(int argc, const char **);
+	const char *helpstr;
+} user_subcommand_t;
+/* General interfaces */
+extern void add_command(user_command_t *cmd);
+void execcmd(int, const char **);
+void exec_string(char *);
+void execsubcmd(user_subcommand_t *, int, const char **);
+void print_usage(char *strhead, user_command_t *);
+void invalid_cmd(const char *cmd_name);
+int init_builtin_cmds(void);
+
+#endif /* _VIVI_COMMAND_H_ */
+
+
---
+++ 提交代码/IAP-radio/IAP_For_NB/Project/IAP/src/stm32f10x_it.c
@@ -0,0 +1,210 @@
+/**
+  ******************************************************************************
+  * @file    IAP/src/stm32f10x_it.c 
+  * @author  MCD Application Team
+  * @version V3.3.0
+  * @date    10/15/2010
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides template for all exceptions handler and peripherals
+  *          interrupt service routine.
+  ******************************************************************************
+  * @copy
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2010 STMicroelectronics</center></h2>
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x_it.h"
+
+/** @addtogroup IAP
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/******************************************************************************/
+/*            Cortex-M3 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+//	printk("hard fault");
+  while (1);
+}
+
+// From Joseph Yiu, minor edits by FVH
+// hard fault handler in C,
+// with stack frame location as input parameter
+// called from HardFault_Handler in file xxx.s
+void HardFault_Handler_c (unsigned int * hardfault_args)
+{
+  unsigned int stacked_r0;
+  unsigned int stacked_r1;
+  unsigned int stacked_r2;
+  unsigned int stacked_r3;
+  unsigned int stacked_r12;
+  unsigned int stacked_lr;
+  unsigned int stacked_pc;
+  unsigned int stacked_psr;
+ 
+  stacked_r0 = ((unsigned long) hardfault_args[0]);
+  stacked_r1 = ((unsigned long) hardfault_args[1]);
+  stacked_r2 = ((unsigned long) hardfault_args[2]);
+  stacked_r3 = ((unsigned long) hardfault_args[3]);
+ 
+  stacked_r12 = ((unsigned long) hardfault_args[4]);
+  stacked_lr = ((unsigned long) hardfault_args[5]);
+  stacked_pc = ((unsigned long) hardfault_args[6]);
+  stacked_psr = ((unsigned long) hardfault_args[7]);
+ 
+  printk ("\n\n[Hard fault handler - all numbers in hex]\n");
+  printk ("R0 = %x\n", stacked_r0);
+  printk ("R1 = %x\n", stacked_r1);
+  printk ("R2 = %x\n", stacked_r2);
+  printk ("R3 = %x\n", stacked_r3);
+  printk ("R12 = %x\n", stacked_r12);
+  printk ("LR [R14] = %x  subroutine call return address\n", stacked_lr);
+  printk ("PC [R15] = %x  program counter\n", stacked_pc);
+  printk ("PSR = %x\n", stacked_psr); 
+  while (1);
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSV_Handler exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+  NVIC_ClearPendingIRQ(SysTick_IRQn);
+}
+#include "main.h"
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void TERM_UART_ISR(void)
+{
+  extern void ringbuf_write(uint8_t c);
+	if(USART_GetITStatus(TERM_UART, USART_IT_RXNE) != RESET)
+  {
+    /* Read one byte from the receive data register */
+    ringbuf_write(USART_ReceiveData(TERM_UART));
+    USART_ClearITPendingBit(TERM_UART, USART_IT_RXNE);
+  }
+}
+/******************************************************************************/
+/*                 STM32F10x Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f10x_xx.s).                                            */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles PPP interrupt request.
+  * @param  None
+  * @retval None
+  */
+/*void PPP_IRQHandler(void)
+{
+}*/
+
+
+/**
+  * @}
+  */ 
+
+/******************* (C) COPYRIGHT 2010 STMicroelectronics *****END OF FILE****/
